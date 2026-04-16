acd0b99b64bd7eab1cf4af8dea56374122392c3a
LAURA KAUANY DOS SANTOS ANACLETO <54000450816@info.local>
2026-04-16 16:27:20 -0300

    Validação da Contact Form

--- Contact Form/js/script.js
+++ Contact Form/js/script.js
@@ -1,114 +1,170 @@
+async function cadastrarContato(objetoContato){
+    console.log(objetoContato);
+     
+    const resposta = fetch("http://localhost:3000/contatos", {
+        method: "POST",
+        body: JSON.stringify(objetoContato),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    });
+}
+
+async function buscarEndereco(cep){
+
+     if(cep.trim().length < 8){
+         alert("O CEP deve ter 8 números");
+        return false;
+     }
+
+    try{
+        aguardandoCampos();
+
+    let retorno = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    let dados = await retorno.json();
+    
+    console.log(dados);
+    console.log(dados.logradouro);
+    console.log(dados.bairro);
+    console.log(dados.localidade);
+    console.log(dados.estado);
+
+    campoDoFormulario = dados.logradouro;
+        document.getElementById("rua").value = dados.logradouro;
+    campoDoFormulario = dados.logradouro;
+        document.getElementById("bairro").value = dados.bairro;
+    campoDoFormulario = dados.logradouro;
+        document.getElementById("cidade").value = dados.localidade;
+    campoDoFormulario = dados.logradouro;
+        document.getElementById("estado").value = dados.estado;
+
+
+}
+    catch(error){
+        console.log(error);
+    }
+}
+
+function aguardandoCampos(){
+    document.getElementById("rua").value = "...";
+    document.getElementById("bairro").value = "...";
+    document.getElementById("cidade").value = "...";
+    document.getElementById("estado").value = "...";
+}
+
+
+
 function validarFormulario(){
 
     let quantidadeErros = 0;
 
-    let nome = document.getElementById("nome");
-    let sobrenome = document.getElementById("sobrenome");
-    let email = document.getElementById("email");
-    let pais = document.getElementById("pais");
-    let ddd = document.getElementById("ddd");
-    let telefone = document.getElementById("numero");
-    let cep = document.getElementById("cep");
-    let rua = document.getElementById("rua");
-    let numeroCasa = document.getElementById("numeroCasa");
-    let complemento = document.getElementById("complemento");
-    let bairro = document.getElementById("bairro");
-    let cidade = document.getElementById("cidade");
-    let estado = document.getElementById("estado");
-    let anotacoes = document.getElementById("anotacoes");
+    let nome = document.getElementById("nome").value;
+    let sobrenome = document.getElementById("sobrenome").value;
+    let email = document.getElementById("email").value;
+    let pais = document.getElementById("pais").value;
+    let ddd = document.getElementById("ddd").value;
+    let telefone = document.getElementById("numero").value;
+    let cep = document.getElementById("cep").value;
+    let rua = document.getElementById("rua").value;
+    let numeroCasa = document.getElementById("numeroCasa").value;
+    let complemento = document.getElementById("complemento").value;
+    let bairro = document.getElementById("bairro").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+    let anotacoes = document.getElementById("anotacoes").value;
 
-    if(nome.value.trim().length == 0){
+    if(nome.trim().length == 0){
         formError("nome");
         quantidadeErros++;
     }else{
         reiniciaBorda("nome");
     }
 
-    if(sobrenome.value.trim().length == 0){
+    if(sobrenome.trim().length == 0){
         formError("sobrenome");
         quantidadeErros++;
     }else{
         reiniciaBorda("sobrenome");
     }
 
-    if(email.value.trim().length == 0){
+    if(email.trim().length == 0){
         formError("email");
         quantidadeErros++;
     }else{
         reiniciaBorda("email");
     }
 
-    if(pais.value.trim().length == 0){
+    if(pais.trim().length == 0){
         formError("pais");
         quantidadeErros++;
     }else{
         reiniciaBorda("pais");
     }
 
-    if(ddd.value.trim().length == 0){
+    if(ddd.trim().length == 0){
         formError("ddd");
         quantidadeErros++;
     }else{
         reiniciaBorda("ddd");
     }
 
-    if(telefone.value.trim().length == 0){
+    if(telefone.trim().length == 0){
         formError("numero");
         quantidadeErros++;
     }else{
         reiniciaBorda("numero");
     }
 
-    if(cep.value.trim().length == 0){
+    if(cep.trim().length == 0){
         formError("cep");
         quantidadeErros++;
     }else{
         reiniciaBorda("cep");
     }
 
-    if(rua.value.trim().length == 0){
+    if(rua.trim().length == 0){
         formError("rua");
         quantidadeErros++;
     }else{
         reiniciaBorda("rua");
     }
 
-    if(numeroCasa.value.trim().length == 0){
+    if(numeroCasa.trim().length == 0){
         formError("numeroCasa");
         quantidadeErros++;
     }else{
         reiniciaBorda("numeroCasa");
     }
 
-    if(complemento.value.trim().length == 0){
+    if(complemento.trim().length == 0){
         formError("complemento");
         quantidadeErros++;
     }else{
         reiniciaBorda("complemento");
     }
 
-    if(bairro.value.trim().length == 0){
+    if(bairro.trim().length == 0){
         formError("bairro");
         quantidadeErros++;
     }else{
         reiniciaBorda("bairro");
     }
 
-    if(cidade.value.trim().length == 0){
+    if(cidade.trim().length == 0){
         formError("cidade");
         quantidadeErros++;
     }else{
         reiniciaBorda("cidade");
     }
 
-    if(estado.value.trim().length == 0){
+    if(estado.trim().length == 0){
         formError("estado");
         quantidadeErros++;
     }else{
         reiniciaBorda("estado");
     }
 
-    if(anotacoes.value.trim().length == 0){
+    if(anotacoes.trim().length == 0){
         formError("anotacoes");
         quantidadeErros++;
     }else{
@@ -118,7 +174,27 @@ function validarFormulario(){
     if(quantidadeErros > 0){
         alert("Existem " + quantidadeErros + " erros no formulário!");
     }else{
-        alert("Formulário enviado com sucesso!");
+
+        let objetoContato = {
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email,
+            pais: pais,
+            ddd: ddd,
+            telefone: telefone,
+            cep: cep,
+            rua: rua,
+            numeroCasa: numeroCasa,
+            complemento: complemento,
+            bairro: bairro,
+            cidade: cidade,
+            estado: estado,
+            anotacoes: anotacoes
+        }
+
+
+        let cadastrado =  cadastrarContato(objetoContato);
+        
         reiniciaTodasAsBordas();
     }
 }
