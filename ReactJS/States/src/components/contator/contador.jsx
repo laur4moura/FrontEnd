05652c10ae3a6bb8702ef371fa6af4b819fa6c73
LAURA KAUANY DOS SANTOS ANACLETO <54000450816@info.local>
2026-05-07 16:48:21 -0300

    React

---
+++ ReactJS/States/src/components/contator/contador.jsx
@@ -0,0 +1,14 @@
+import "./contador.css";
+
+const Contador = () => {
+    const [valor, setValor] = useState(0);
+  return (
+    <>
+        <p>Contagem: {valor}</p>
+        <button onClick={() => {return setValor(valor + 1)}}>++</button>
+    
+    </>
+    );
+};
+
+export default Contador;
